42077e5b604693bdfb6437968c14e7a79c522f81
Saraholoughlin5 <saraholoughlin5@gmail.com>
2021-03-14 18:33:05 +0000

    Active map link button set up (JS). URL links prep.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
+// Automatic active class for button 1
+$("#button-one").toggleClass("active");
+var increment = 0;
+
 // Declaring Global variable "Burren" Object.
 const myObj = {
     burren: {
-        restaurants: ['Bofeys', 52.94537, -9.06517, 'Monks', 53.11988, -9.15470, 'Wild Atlantic Lodge', 53.11598, -9.14812, 'Tea & Garden Rooms', 53.11906, -9.15373, 'Red Onion', 52.99014, -9.21973], 
-        accommodations: ["The Wild Atlantic Lodge", 53.11595, -9.14816, "Vaughan Lodge Hotel", 52.93323, -9.34100, "Ballinalacken Castle", 53.04661, -9.33783, "Sheedy's Country House",53.03076, -9.28944, "Doolin Inn", 53.01280, -9.38368],
-        attractions: ['Ailwee Cave', 53.08926, -9.14367, 'Doolin Cliff Walks', 52.99854, -9.38787, 'Burren Birds of Prey Centre', 53.08936, -9.14394, 'Cliffs of Moher Geopark', 52.97236, -9.42632, 'The Burren Centre', 52.99043, -9.21658]
+        restaurants: ["Bofey Quinn's Bar & Lodge, Corofin", 52.94537, -9.06517, "Monks Seafood Restaurant & Bar, Ballyvaughan", 53.11988, -9.15470, "The Wild Atlantic Lodge, Ballyvaughan", 53.11598, -9.14812, "Tea & Garden Rooms, Ballyvaughan", 53.11906, -9.15373, "Vaughan's Pub, Kilfenora", 52.99014, -9.21974], 
+        accommodations: ["The Wild Atlantic Lodge, Ballyvaughan", 53.11595, -9.14816, "Vaughan Lodge Hotel, Lahinch", 52.93323, -9.34100, "Ballinalacken Castle, Country House Hotel, Doolin", 53.04661, -9.33783, "Sheedy's Country House Hotel, Lisdoonvarna",53.03076, -9.28944, "Doolin Inn, Doolin", 53.01280, -9.38368],
+        attractions: ["Poulnabrone Dolmen, Poulnabrone", 53.04879, -9.14007, "Doolin Cliff Walks, Doolin", 52.99854, -9.38787, "Burren Birds of Prey Centre, Ballyvaughan", 53.08936, -9.14394, "Cliffs of Moher Geopark, Inis Díomáin", 52.97236, -9.42632, "The Burren Centre, Kilfenora", 52.99043, -9.21658]
     }
 }   
 
@@ -13,22 +17,30 @@ var myVar8; var myVar8; var myVar9; var myVar10; var myVar11; var myVar12; var m
 
 // function to change between restaurants, accommodations and attractions.
 function initChoice(choices) {
-        return (myVar0 = Object.keys(myObj).map((key) => myObj[key][choices][0]),
-        myVar1 = Object.keys(myObj).map((key) => myObj[key][choices][1])[0],
-        myVar2 = Object.keys(myObj).map((key) => myObj[key][choices][2])[0],
-        myVar3 = Object.keys(myObj).map((key) => myObj[key][choices][3]),
-        myVar4 = Object.keys(myObj).map((key) => myObj[key][choices][4])[0],
-        myVar5 = Object.keys(myObj).map((key) => myObj[key][choices][5])[0],
-        myVar6 = Object.keys(myObj).map((key) => myObj[key][choices][6]),
-        myVar7 = Object.keys(myObj).map((key) => myObj[key][choices][7])[0],
-        myVar8 = Object.keys(myObj).map((key) => myObj[key][choices][8])[0],
-        myVar9 = Object.keys(myObj).map((key) => myObj[key][choices][9]),
-        myVar10 = Object.keys(myObj).map((key) => myObj[key][choices][10])[0],
-        myVar11 = Object.keys(myObj).map((key) => myObj[key][choices][11])[0],
-        myVar12 = Object.keys(myObj).map((key) => myObj[key][choices][12]),
-        myVar13 = Object.keys(myObj).map((key) => myObj[key][choices][13])[0],
-        myVar14 = Object.keys(myObj).map((key) => myObj[key][choices][14])[0]),
-        changeDivTxt();
+    if (increment == 0) {
+        if (choices == 'accommodations') {
+            $("#button-one").toggleClass("active");
+        } else if (choices == 'attractions') {
+            $("#button-one").toggleClass("active");
+        } 
+    }
+    return (myVar0 = Object.keys(myObj).map((key) => myObj[key][choices][0]),
+    myVar1 = Object.keys(myObj).map((key) => myObj[key][choices][1])[0],
+    myVar2 = Object.keys(myObj).map((key) => myObj[key][choices][2])[0],
+    myVar3 = Object.keys(myObj).map((key) => myObj[key][choices][3]),
+    myVar4 = Object.keys(myObj).map((key) => myObj[key][choices][4])[0],
+    myVar5 = Object.keys(myObj).map((key) => myObj[key][choices][5])[0],
+    myVar6 = Object.keys(myObj).map((key) => myObj[key][choices][6]),
+    myVar7 = Object.keys(myObj).map((key) => myObj[key][choices][7])[0],
+    myVar8 = Object.keys(myObj).map((key) => myObj[key][choices][8])[0],
+    myVar9 = Object.keys(myObj).map((key) => myObj[key][choices][9]),
+    myVar10 = Object.keys(myObj).map((key) => myObj[key][choices][10])[0],
+    myVar11 = Object.keys(myObj).map((key) => myObj[key][choices][11])[0],
+    myVar12 = Object.keys(myObj).map((key) => myObj[key][choices][12]),
+    myVar13 = Object.keys(myObj).map((key) => myObj[key][choices][13])[0],
+    myVar14 = Object.keys(myObj).map((key) => myObj[key][choices][14])[0], 
+    increment = increment + 1),
+    changeDivTxt();
 }
 
 // Change div text
